7fb46f56e28b04de3610a30be99427aab3f3a40f
Marton Nika <marton.nika@gmail.com>
2025-05-01 12:35:14 +0200

    Fix time_driver issues

--- embassy-silabs-project/src/main.rs
+++ embassy-silabs-project/src/main.rs
@@ -12,10 +12,12 @@ use {defmt_rtt as _, panic_probe as _}; // global logger
 async fn main(spawner: Spawner) {
     info!("Initialize peripherals");
     let p = embassy_silabs::init();
-    let led = Output::new(p.PA_04, Level::Low);
-    let led2 = Output::new(p.PA_07, Level::Low);
-    unwrap!(spawner.spawn(blink_1(led)));
-    unwrap!(spawner.spawn(blink_2(led2)));
+    let led0 = Output::new(p.PA_04, Level::Low);
+    let led1 = Output::new(p.PA_07, Level::Low);
+    //let led0 = Output::new(p.PB_02, Level::Low);
+    //let led1 = Output::new(p.PB_04, Level::Low);
+    unwrap!(spawner.spawn(blink_1(led0)));
+    unwrap!(spawner.spawn(blink_2(led1)));
 }
 
 #[embassy_executor::task]
@@ -24,7 +26,7 @@ async fn blink_1(mut led: Output<'static>) {
         let now = Instant::now();
         info!("Blink 1 triggered at {}", now.as_millis());
         led.toggle();
-        Timer::after_millis(500).await;
+        Timer::after_millis(1500).await;
     }
 }
 
@@ -34,6 +36,6 @@ async fn blink_2(mut led: Output<'static>) {
         let now = Instant::now();
         info!("Blink 2 triggered at {}", now.as_millis());
         led.toggle();
-        Timer::after_millis(750).await;
+        Timer::after_millis(2000).await;
     }
 }
--- embassy-silabs/src/time_driver.rs
+++ embassy-silabs/src/time_driver.rs
@@ -6,7 +6,7 @@ use embassy_sync::blocking_mutex::CriticalSectionMutex as Mutex;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_time_driver::Driver;
 use embassy_time_queue_utils::Queue;
-use silabs_pac::sysrtc0_ns::regs::Grp0Ctrl;
+use silabs_pac::sysrtc0_ns::regs::{Grp0Ctrl, Grp0If};
 
 use crate::interrupt::InterruptExt;
 use crate::{interrupt, pac};
@@ -33,7 +33,7 @@ fn rtc() -> pac::sysrtc::ns {
 /// a new period start has raced us between reading `period` and `counter`, so we assume the `counter` value
 /// corresponds to the next period.
 ///
-/// `period` is a 32bit integer, so It overflows on 2^32 * 2^31 / 32768 seconds of uptime.
+/// `period` is a 32bit integer, so It overflows on 2^32 * 2^31  seconds of uptime.
 /// 8.9 billion years might actually be a bit unnecessary.
 fn calc_now(period: u32, counter: u32) -> u64 {
     ((period as u64) << 31) + ((counter ^ ((period & 1) << 31)) as u64)
@@ -53,17 +53,17 @@ impl AlarmState {
     }
 }
 
-struct RtcDriver {
-    /// Number of 2^23 periods elapsed since boot.
+pub(crate) struct RtcDriver {
+    /// Number of 2^31 periods elapsed since boot.
     period: AtomicU32,
     /// Timestamp at which to fire alarm. u64::MAX if no alarm is scheduled.
-    alarms: Mutex<AlarmState>,
+    alarm: Mutex<AlarmState>,
     queue: Mutex<RefCell<Queue>>,
 }
 
 embassy_time_driver::time_driver_impl!(static DRIVER: RtcDriver = RtcDriver {
     period: AtomicU32::new(0),
-    alarms: Mutex::const_new(CriticalSectionRawMutex::new(), AlarmState::new()),
+    alarm: Mutex::const_new(CriticalSectionRawMutex::new(), AlarmState::new()),
     queue: Mutex::new(RefCell::new(Queue::new())),
 });
 
@@ -91,18 +91,18 @@ impl RtcDriver {
         // "The normal operation of SYSRTC is to configure it, enable it, start it, and then leave it running"
         // Config
         // Select LFRCO as the clock source
+        // Maybe unnecessary
         pac::CMU
             .sysrtc0clkctrl()
             .write(|w| w.set_clksel(pac::cmu_ns::vals::Sysrtc0clkctrlClksel::LFRCO));
-        // Set a compare to 0x8000_0000
+        // Set a compare to 0x8000_0000 - half overflow
         r.grp0_cmp0value().write(|w| w.set_cmp0value(0x8000_0000));
         // Enable the comparison and overflow interrupts
         r.grp0_ien().write(|w| {
             w.set_ovf(true);
             w.set_cmp0(true)
         });
-        // Enable compares once and leave them running
-        r.grp0_ctrl().write_value(Grp0Ctrl(0b11));
+        // Enable compares once and leave them running - can be disabled by grp0_ien()
         r.grp0_ctrl().write(|w| {
             w.set_cmp0en(true);
             w.set_cmp1en(true)
@@ -124,18 +124,18 @@ impl RtcDriver {
         let r = rtc();
         // Overflow
         if r.grp0_if().read().ovf() {
-            r.grp0_if().write(|w| w.set_ovf(false));
+            r.grp0_if_clr().write(|w| w.set_ovf(true));
             self.next_period();
         }
         // Compare 0
         if r.grp0_if().read().cmp0() {
-            r.grp0_if().write(|w| w.set_cmp0(false));
+            r.grp0_if_clr().write(|w| w.set_cmp0(true));
             self.next_period();
         }
 
         // Compare 1
         if r.grp0_if().read().cmp1() {
-            r.grp0_if().write(|w| w.set_cmp1(false));
+            r.grp0_if_clr().write(|w| w.set_cmp1(true));
             critical_section::with(|cs| {
                 self.trigger_alarm(cs);
             });
@@ -149,24 +149,17 @@ impl RtcDriver {
             self.period.store(period, Ordering::Relaxed);
             let t = (period as u64) << 31;
 
-            let alarm = &self.alarms.borrow(cs);
+            let alarm = &self.alarm.borrow(cs);
             let at = alarm.timestamp.get();
 
             if at < t + 0xc000_0000 {
                 // just enable it. `set_alarm` has already set the correct CC val.
-                r.grp0_ien().write(|w| w.set_cmp1(true));
+                r.grp0_ien().modify(|w| w.set_cmp1(true));
             }
         })
     }
 
     fn trigger_alarm(&self, cs: CriticalSection) {
-        let r = rtc();
-        r.grp0_ien().write(|w| w.set_cmp1(true));
-
-        let alarm = &self.alarms.borrow(cs);
-        alarm.timestamp.set(u64::MAX);
-
-        // Call after clearing alarm, so the callback can set another alarm.
         let mut next = self
             .queue
             .borrow(cs)
@@ -182,54 +175,53 @@ impl RtcDriver {
     }
 
     fn set_alarm(&self, cs: CriticalSection, timestamp: u64) -> bool {
-        let alarm = &self.alarms.borrow(cs);
+        let alarm = &self.alarm.borrow(cs);
         alarm.timestamp.set(timestamp);
 
         let r = rtc();
 
-        loop {
-            let t = self.now();
-            if timestamp <= t {
-                // If alarm timestamp has passed the alarm will not fire.
-                // Disarm the alarm and return `false` to indicate that.
-                r.grp0_ien().write(|w| w.set_cmp1(false));
+        let t = self.now();
+        if timestamp <= t {
+            // If alarm timestamp has passed the alarm will not fire.
+            // Disarm the alarm and return `false` to indicate that.
+            r.grp0_ien().modify(|w| w.set_cmp1(false));
 
-                alarm.timestamp.set(u64::MAX);
+            alarm.timestamp.set(u64::MAX);
 
-                return false;
-            }
-
-            // If it hasn't triggered yet, setup it in the compare channel.
-
-            // Write the CC value regardless of whether we're going to enable it now or not.
-            // This way, when we enable it later, the right value is already set.
-
-            // "Note that when setting the compare value to the current counter value, a compare event
-            // may not get generated until the counter overflows and reaches the current value again.
-            // To generate a compare event quickly, it is recommended to program the compare value to
-            // the current counter value + 1. "
-
-            // So, let a safe timestamp be +2
-            let safe_timestamp = timestamp.max(t + 2);
-            r.grp0_cmp1value()
-                .write(|w| w.set_cmp1value(safe_timestamp as u32));
-
-            let diff = timestamp - t;
-            if diff < 0xc000_0000 {
-                r.grp0_ien().write(|w| w.set_cmp1(true));
-
-                // If we have not passed the timestamp, we can be sure the alarm will be invoked. Otherwise,
-                // we need to retry setting the alarm.
-                if self.now() + 2 <= timestamp {
-                    return true;
-                }
-            } else {
-                // If it's too far in the future, don't setup the compare channel yet.
-                // It will be setup later by `next_period`.
-                r.grp0_ien().write(|w| w.set_cmp1(false));
-                return true;
-            }
+            return false;
         }
+
+        // If it hasn't triggered yet, setup it in the compare channel.
+
+        // Write the CC value regardless of whether we're going to enable it now or not.
+        // This way, when we enable it later, the right value is already set.
+
+        // "Note that when setting the compare value to the current counter value, a compare event
+        // may not get generated until the counter overflows and reaches the current value again.
+        // To generate a compare event quickly, it is recommended to program the compare value to
+        // the current counter value + 1. "
+        // So, let a safe timestamp be +2
+        let safe_timestamp = timestamp.max(t + 2);
+        r.grp0_cmp1value()
+            .write(|w| w.set_cmp1value(safe_timestamp as u32));
+
+        // Enable it if it'll happen soon. Otherwise, `next_period` will enable it.
+        let diff = timestamp - t;
+        r.grp0_ien().modify(|w| w.set_cmp1(diff < 0xc000_0000));
+
+        // Reevaluate if the alarm timestamp is still in the future
+        if self.now() + 2 >= timestamp {
+            // If alarm timestamp has passed since we set it, we have a race condition and
+            // the alarm may or may not have fired.
+            // Disarm the alarm and return `false` to indicate that.
+            // It is the caller's responsibility to handle this ambiguity.
+            r.grp0_ien().modify(|w| w.set_cmp1(false));
+            alarm.timestamp.set(u64::MAX);
+            return false;
+        }
+
+        // We're confident the alarm will ring in the future.
+        true
     }
 }
 
